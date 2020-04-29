355cda60c5ebe260a535e2cd8952beabcfb361ba
Adam Niehenke <a.niehenke@setonhill.edu>
2020-04-29 16:00:08 -0400

    Initial Commit

---
+++ Niehenke - Location/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Niehenke - Location
+//
+//  Created by Adam Niehenke on 4/29/20.
+//  Copyright © 2020 Adam Niehenke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
